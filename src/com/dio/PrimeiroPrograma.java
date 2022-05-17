ce1da141c5a5885fe30a85dca7fb6a18d71285a2
Arlicie <vbdlemp4@gmail.com>
2022-05-17 18:07:08 -0400

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		/*int a=3, b=2;
+		System.out.println("Hello World!" + (a+b));
+	} */
+	}
+}
+	
+class Livros{
+	private String nome, npag;
+}
+
